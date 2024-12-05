80fc42a8b3b5293da7180f51e9cdcd0375db7825
2180608992_Nguyễn Đào Anh Tuấn <anktuan345@gmail.com>
2024-12-05 17:14:13 +0700

    thay đổi model

---
+++ HTQuanLyHoSoSucKhoe/HTQuanLyHoSoSucKhoe/Models/Appointment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HTQuanLyHoSoSucKhoe.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }  // Chỉ giữ lại UserId
+
+        [ForeignKey("BenhVien")]
+        public int BenhVienId { get; set; }  // Chỉ giữ lại BenhVienId
+
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone_Number { get; set; }
+        public string TenBenhVien { get; set; }
+        public DateTime Appointment_Date { get; set; }
+        public DateTime Created_At { get; set; }
+        public DateTime Updated_At { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual BenhVien BenhVien { get; set; }
+    }
+}
