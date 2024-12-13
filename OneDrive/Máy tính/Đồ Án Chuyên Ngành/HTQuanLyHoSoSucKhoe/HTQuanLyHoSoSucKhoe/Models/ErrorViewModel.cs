9f3ba5bc9e19ad9252529e8488240e3b8d9b8b67
2180608992_Nguyễn Đào Anh Tuấn <anktuan345@gmail.com>
2024-12-13 15:01:52 +0700

    remove onedrive

--- OneDrive/Máy tính/Đồ Án Chuyên Ngành/HTQuanLyHoSoSucKhoe/HTQuanLyHoSoSucKhoe/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HTQuanLyHoSoSucKhoe.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
