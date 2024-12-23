859a8d8c703efa8d4bc479696782ddf099847517
2180608992_Nguyễn Đào Anh Tuấn <anktuan345@gmail.com>
2024-12-23 14:44:48 +0700

    thang hon luy

---
+++ HTQuanLyHoSoSucKhoe/HTQuanLyHoSoSucKhoe/Controllers/DonThuocController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HTQuanLyHoSoSucKhoe.Controllers
+{
+    [Authorize(Roles = "User")]
+    public class DonThuocController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HTQuanLyHoSoSucKhoe/HTQuanLyHoSoSucKhoe/Controllers/QuanLyDonThuocController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HTQuanLyHoSoSucKhoe.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class QuanLyDonThuocController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
